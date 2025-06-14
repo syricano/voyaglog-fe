fba7180b5a7a03f18e71d2666eab45798269e8bf
Anas <syriacano@gmail.com>
2025-06-14 12:12:04 +0200

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="container">
+        <h1>Welcome to the front end server</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
